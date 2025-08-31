c2266d101b86cc07d4132a3422f0419c162cac1a
fran utibe joseph <majesticfrank7@gmail.com>
2025-08-31 20:49:37 +0100

    created a superuser and authntication service

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    # Show these fields in the list view
+    list_display = ('title', 'author', 'publication_year')
+
+    # Add filters (sidebar)
+    list_filter = ('publication_year', 'author')
+
+    # Add a search bar
+    search_fields = ('title', 'author')
+
+   
+    list_editable = ('author', 'publication_year')
+
+    
+
+# Register with custom configuration
+admin.site.register(Book, BookAdmin)
