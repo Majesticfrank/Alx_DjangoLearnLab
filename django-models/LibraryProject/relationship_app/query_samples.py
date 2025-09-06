42e205961014cb9e0b84cfb5444f70f2eec3be7b
fran utibe joseph <majesticfrank7@gmail.com>
2025-09-06 02:37:00 +0100

    relationships in django

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,32 @@
+import os 
+import django
+from django.apps import apps
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")
+django.setup()
+
+from relationship_app.models import Author, Book, Library, Librarian
+
+
+# 1. Query all books
+books = Book.objects.all()
+for book in books:
+    print(book.title, book.author)
+
+
+# 2. Books by specific author 
+
+specific_author = Book.objects.filter(author="chinua Achebe")
+for book in specific_author:
+    print(book.title)
+
+
+# 3. Books in Library
+library = Library.objects.get(name="Central Library")
+for book in library.books.all():
+    print(book.title)
+
+
+# 4. Librarian of a library 
+librarian =library.librarian 
+print(librarian.name)
