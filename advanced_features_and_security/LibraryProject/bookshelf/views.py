ffa7a6334d04799491a7012791edb81a99245235
fran utibe joseph <majesticfrank7@gmail.com>
2025-09-15 03:36:39 +0100

    security

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from django.shortcuts import render, get_object_or_404
+
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import BookForm
+
 
 # Create your views here.
-
-
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
@@ -28,3 +29,12 @@ def delete_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     book.delete()
     return render(request, "bookshelf/book_deleted.html")
+def create_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
