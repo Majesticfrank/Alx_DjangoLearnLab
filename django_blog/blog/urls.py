7b0407102aa311b8255009346c74faf353ca0859
fran utibe joseph <majesticfrank7@gmail.com>
2025-10-05 16:28:26 +0100

    added tag features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -29,7 +29,7 @@ urlpatterns=[
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='edit-comment'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete-comment'),
     path('search/', views.search_posts, name='search-posts'),
-    path('tags/<str:tag_name>/', views.posts_by_tag, name='posts-by-tag'),
+   path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='posts-by-tag'),
 
 
 
